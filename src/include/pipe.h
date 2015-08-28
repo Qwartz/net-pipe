60ccb98a0173d1eb3e8c9abd494a614b9ceaa6ef
Corbin Matschull <c.matschull@icloud.com>
2015-08-28 14:28:04 -0600

    Code

--- src/include/pipe.h
+++ src/include/pipe.h
@@ -1,6 +1,17 @@
+/**
+ * \file pipe.h
+ * @version 1.0
+ */
 #ifndef PIPE_H_
 #define PIPE_H_
 
+#include <stdio.h>
 
+struct pipe_sizes
+{
+    unsigned short pipe16_t;
+    unsigned int   pipe32_t;
+    unsigned long  pipe64_t;
+} pipe_t;
 
 #endif
